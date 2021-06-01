72aeb9b30022f53ab8834206a5889ff3bd672e9e
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-01 16:45:30 +0300

    add

---
+++ 000-intro/app.js
@@ -0,0 +1,6 @@
+window.onload = function (){
+document.querySelector("h1").style.color="red";
+};
+
+//or.. if the already code is located down of page..
+//document.querySelector("h1").style.color="red";
---
+++ 000-intro/page.js
@@ -0,0 +1,3 @@
+document.write("<h1>home page</h1>");
+document.querySelector("h1").style.color = 'red';
+document.querySelector("h1").style.fontSize = '80px';

985f828957ea18d504d69c9b012a143de8301035
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-17 17:57:20 +0300

    add

---
+++ 000-web-app/001-async-await/app.js
@@ -0,0 +1,39 @@
+/*
+async - await
+
+*/
+
+const users = [
+    {id:1, name: 'fua'},
+    {id:2, name: 'mua'},
+    {id:3, name: 'nata'},
+]
+
+const articles = [
+    {userId:1, articles: ['fua', 'mua', 'nata']},
+    {userId:2, articles: ['one', 'two', 'three']},
+    {userId:3, articles: ['four', 'five', 'six']},
+]
+
+function getUser(name) {
+    return new Promise((resolve, reject) => {
+        const user = users.find((user) => user.name === name)
+
+        if(user){
+            return resolve(user)
+        }else{
+            reject(`no such user with name: ${name}`)
+        }
+    })
+}
+
+function getArticles(userId){
+    return new Promise((resolve, reject) =>{
+        const userArticles = articles.find(user) => user.userId === userId)
+        if(userArticles){
+            return resolve(userArticles.articles)
+        }else{
+             reject(`Wrong ID`)
+        }
+    })
+}
---
+++ 000-web-app/001-async-await/readme.md
@@ -0,0 +1,20 @@
+---
+stack: js
+lang: all
+---
+
+## async and await
+async and await allow us to write asunc code in sunchronous fashion,
+insted of endlessly nesting callbacks.
+and the end point are following where when we use async await always
+tait till the promise is settled.
+async must be present, always return a promise
+await will waits till promise is settled.
+error handling - try and catch block
+try{
+
+}catch{
+
+}
+
+---

da7eefee7bf86e144c823441dfc61fcc000f68fe
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-01 20:13:13 +0300

    add

---
+++ 000-intro/readme.md
@@ -0,0 +1,11 @@
+---
+stack: js
+lang: all
+---
+
+## ES 
+Ecma Script
+[wiki](https://en.wikipedia.org/wiki/ECMAScript)
+
+also checkout:
+[es6 features](es6-features.org)
---
+++ 004-more-syntax/001-Typeof/app.js
@@ -0,0 +1,9 @@
+console.log("nancy");
+console.log(typeof "nancy");
+console.log(typeof 1);
+console.log(typeof [10,20,30,40,50]);
+console.log(typeof ["fu","mu","na"]);
+console.log(typeof {name: 'mu', age: 21});
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);

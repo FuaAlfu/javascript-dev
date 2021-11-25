91679c0385b0612140b8880622249b051f3d1e58
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-11-25 04:19:14 +0300

    add

---
+++ 005-algo/002-sorting-data/002-selectionSort/dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+function sort(array) {
+    for (let x = 0; x < array.length - 1; x++) {
+        let min = x;
+        for (let y = x + 1; y < array.length; y++) {
+            if (array[y] < array[min]) {
+                //pick up the less
+                min = y;
+            }
+        }
+        const temp = array[min];
+        array[min] = array[x];
+        array[x] = temp;
+    }
+    return array;
+}
+let arr = [44, 1, 89, 555, 21, 5, 34, 1];
+console.log(sort(arr));

29574c54ac48cfa63ddf35bd162b2d74c3be02c8
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-12-28 15:54:27 +0300

    add

---
+++ 005-algo/002-sorting-data/005-mergeSort/src/app.ts
@@ -0,0 +1,35 @@
+function mergeSort<T>(input: T[]): T[] {
+
+    if (input.length <= 1) {
+        return input;
+    }
+
+    const listA = input.slice(0, Math.ceil(input.length / 2));
+    const listB = input.slice(listA.length, input.length);
+
+    return sortedMerge(
+        mergeSort(listA),
+        mergeSort(listB),
+    )
+}
+
+function sortedMerge<T>(listA: T[], listB: T[]): T[] {
+
+    const merged: T[] = [];
+
+    while (listA.length && listB.length) {
+
+        if (listA[0] <= listB[0])
+            merged.push(listA.shift());
+        else
+            merged.push(listB.shift());
+    }
+
+    while (listA.length > 0) merged.push(listA.shift());
+    while (listB.length > 0) merged.push(listB.shift());
+
+    return merged;
+}
+
+let arr = [44,1,89,555,21,5,34,1]
+console.log(mergeSort(arr));

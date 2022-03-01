0266595ea76ce4c47325840472c67b54f3d96469
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-03-02 02:56:59 +0300

    add

---
+++ 004-more-syntax/004-stuff/006-f/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const generatPass = () => {
+    const digits = "0123456789";
+    let p = "";
+    for (let i = 0; i < 7; i++) {
+        p += digits[Math.floor(Math.random() * 10)];
+    }
+    return p;
+};
+console.log(generatPass());

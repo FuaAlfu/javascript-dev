4519f060aa6948abe7ddd8491d7f0260851e92cf
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-03-03 07:00:14 +0300

    add

---
+++ 004-more-syntax/004-stuff/007-js-short-hand/app.js
@@ -0,0 +1,16 @@
+const data = {name: "oni"}
+const res = {data: data}
+const res2 = {data}
+
+console.log(res);
+console.log(res2);
+
+//-------------
+
+const name = 'oni';
+const title = 'akuma';
+const obj = {name: name, title: title}
+const obj2 = {name, title}
+
+console.log(obj);
+console.log(obj2);

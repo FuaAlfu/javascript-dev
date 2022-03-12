08d828966d9282665e2325de591db7d68be8482f
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-03-12 21:57:48 +0300

    add

---
+++ 004-more-syntax/004-stuff/008-g/app.js
@@ -0,0 +1,13 @@
+for(let i = 0; i <= 1; i++){
+    console.log('f');
+    for(let j = 0; j <= 1; j++){
+            console.log('u');
+            for(let y = 0; y < 1; y--){
+                console.log('a');
+                console.log('---');
+                if(y < 0){
+                    break;
+                }
+            }
+    }
+}
